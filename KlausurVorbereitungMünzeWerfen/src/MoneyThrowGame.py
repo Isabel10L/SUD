7402b4f834ca2a6eb702804e8c4eb2c60de96c20
Isabel <isabel.l2@gso.schule.koeln>
2022-06-05 14:55:22 +0200

    Verschönerung des Münz-Spiels

--- KlausurVorbereitungMünzeWerfen/src/MoneyThrowGame.py
+++ KlausurVorbereitungMünzeWerfen/src/MoneyThrowGame.py
@@ -5,17 +5,14 @@
 import random
 
 print("Willkommen beim Münzwurf!")
-print("")
-throw = int(input("Wie viele Würfe wollen Sie machen?")) + 1
+user_input_throw = int(input("Wie viele Würfe wollen Sie machen?")) + 1
 try_throw = 0
 
-for throw_repeat in range(1, throw):
-
+for throw_repeat in range(1, user_input_throw):
     result = random.randint(1, 2)
     if result == 1:
         try_throw = try_throw + 1
         print(str(try_throw) + ": Kopf")
-
     else:
         try_throw = try_throw + 1
         print(str(try_throw) + ": Zahl")
