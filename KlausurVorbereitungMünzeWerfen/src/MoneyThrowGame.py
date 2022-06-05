dbf4be96e235a3106782a46513032f45f8f663e2
Isabel <isabel.l2@gso.schule.koeln>
2022-06-05 15:05:22 +0200

    Erweitert: Welches Ergebnis wie oft kam und wer gewonnen oder gleichstand hat

--- KlausurVorbereitungMünzeWerfen/src/MoneyThrowGame.py
+++ KlausurVorbereitungMünzeWerfen/src/MoneyThrowGame.py
@@ -7,12 +7,26 @@ import random
 print("Willkommen beim Münzwurf!")
 user_input_throw = int(input("Wie viele Würfe wollen Sie machen?")) + 1
 try_throw = 0
+count_head = 0
+count_number = 0
 
 for throw_repeat in range(1, user_input_throw):
     result = random.randint(1, 2)
     if result == 1:
+        count_head = count_head + 1
         try_throw = try_throw + 1
         print(str(try_throw) + ": Kopf")
     else:
+        count_number = count_number + 1
         try_throw = try_throw + 1
         print(str(try_throw) + ": Zahl")
+
+print("Anzahl des Ergebnisses 'Kopf': " + str(count_head))
+print("Anzahl des Ergebnisses 'Zahl': " + str(count_number))
+
+if count_head > count_number:
+    print("Kopf hat gewonnen!")
+elif count_head < count_number:
+    print("Zahl hat gewonnen!")
+else:
+    print("Es ist gleichstand.")
