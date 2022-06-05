3bc9df7d23cdfa1b9bf8ed8747c7869a41e0a614
Isabel <isabel.l2@gso.schule.koeln>
2022-06-05 15:16:47 +0200

    Schwierigkeiten beim Testen

---
+++ KlausurVorbereitungMünzeWerfen/test/MoneyThrowGameUnitTest.py
@@ -0,0 +1,3 @@
+# Schwierig zu testen Grund 1: User- Input müsste gemockt werden
+# Schwierig zu testen Grund 1: Random-Funktion muss zum testen rausgenommen werden,
+# da es nicht genau vorhersehbar ist, welches Ergebnis rauskommt
