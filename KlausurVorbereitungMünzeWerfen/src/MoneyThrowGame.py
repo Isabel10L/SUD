8851b5b9d1e0b358865ece27cefa1b206d67e802
Isabel <isabel.l2@gso.schule.koeln>
2022-06-05 16:10:45 +0200

    verbessert

--- KlausurVorbereitungMünzeWerfen/src/MoneyThrowGame.py
+++ KlausurVorbereitungMünzeWerfen/src/MoneyThrowGame.py
@@ -1,24 +1,24 @@
-# Münze hat zwei seiten
-# Eingabe, wie viele Würfe man machen kann
-# Ergebnis pro Wurf in Konsole eingeben
-# Stichwort: random
+# Münze hat zweiseiten; Eingabe: wie viele Würfe man machen kann; Wiedergabe: Ergebnis pro Wurf; Stichwort: random
 import random
 
 print("Willkommen beim Münzwurf!")
 print("Wie viele Würfe wollen Sie machen?")
 user_input_throw = int(input("Tragen Sie dazu eine ganze Zahl ein: ")) + 1
-try_throw = 0
-count_head = 0
-count_number = 0
 
+# VARIABLEN DELKARATION
+try_throw = 0
+head = 0
+number = 0
+
+# Durchführung des Spiels (Spiellogik)
 for throw_repeat in range(1, user_input_throw):
     result = random.randint(1, 2)
     if result == 1:
-        count_head = count_head + 1
+        head = head + 1
         try_throw = try_throw + 1
         print(str(try_throw) + ": Kopf")
     else:
-        count_number = count_number + 1
+        number = number + 1
         try_throw = try_throw + 1
         print(str(try_throw) + ": Zahl")
 
@@ -27,11 +27,11 @@ def winner_result(count_head, count_number):
     print("Anzahl des Ergebnisses 'Kopf': " + str(count_head))
     print("Anzahl des Ergebnisses 'Zahl': " + str(count_number))
     if count_head > count_number:
-        return "Kopf hat gewonnen!"
+        print("Kopf hat gewonnen!")
     elif count_head < count_number:
-        return "Zahl hat gewonnen!"
+        print("Zahl hat gewonnen!")
     else:
-        return "Es ist gleichstand."
+        print("Es ist gleichstand.")
 
 
-winner_result(count_head, count_number)
+winner_result(head, number)
